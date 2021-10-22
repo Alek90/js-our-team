7f083e73819daf2fe2ad780ab853446a0db76439
Alek90 <4lek.90@gmail.com>
2021-10-22 13:42:17 +0200

    Creata repo.
    Creata repo, creati file, legati file. Creato ourTeam, da vedere come inserire le immagini.

---
+++ assets/main.js
@@ -0,0 +1,34 @@
+/* OBBIETTIVI:
+    Rappresentare una lista di membri appartenenti ad un team.
+    Ogni membro deve possedere caratteristiche prestabilite: name; role; image.
+    Mandare in console gli elementi del team con le proprietà.     
+*/
+
+
+const ourTeam = [
+    {
+        'name': 'Roger',
+        'role': 'team_leader',
+        'image': 'img//picsum.photos/200'
+    },
+
+    {
+        'name': 'Fluf',
+        'role': 'autista',
+        'image': 'https://picsum.photos/200'
+    },
+
+    {
+        'name': 'Dodge',
+        'role': 'operatore_dae',
+        'image': 'webkitURLhttps//picsum.photos/200'
+    },
+
+    {
+        'name': 'Roger',
+        'role': 'operatore_livello_avanzato',
+        'image': 'img'
+    }
+];
+
+console.log(ourTeam);
